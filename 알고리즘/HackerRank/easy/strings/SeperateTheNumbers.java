b19822ce1814ba2d48fc7ce1019807d99c75c704
Miji Park <mijip0320@gmail.com>
2021-02-10 10:31:43 +0900

    update SeperateTheNumbers.java

---
+++ 알고리즘/HackerRank/easy/strings/SeperateTheNumbers.java
@@ -0,0 +1,19 @@
+package com.example.Algorithm.easy.strings;
+
+public class SeperateTheNumbers {
+    // Complete the separateNumbers function below.
+    static void separateNumbers(String s) {
+
+
+    }
+
+
+    public static void main(String[] args) {
+
+            String s ="99100";
+
+            separateNumbers(s);
+
+    }
+
+}
