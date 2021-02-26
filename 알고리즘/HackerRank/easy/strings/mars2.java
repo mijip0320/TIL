1d63df4e812ee98dea647fe1122e6c1e26654b37
Miji Park <mijip0320@gmail.com>
2021-02-26 09:58:45 +0900

    update algorithm

---
+++ 알고리즘/HackerRank/easy/strings/mars2.java
@@ -0,0 +1,38 @@
+package com.example.Algorithm.easy.strings;
+
+public class mars2 {
+
+    // Complete the marsExploration function below.
+    static int marsExploration(String s) {
+        String sos = "SOS";
+        int answer = 0;
+
+
+
+
+
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i) != sos.charAt(i%3)){
+                System.out.println("sos.charAt(i%3) = "+sos.charAt(i%3));
+
+                answer++;
+            }
+        }
+
+
+        return answer;
+
+    }
+
+
+    public static void main(String[] args) {
+
+
+        String s = "SOSSOT";
+
+        int result = marsExploration(s);
+
+        System.out.println(result);
+
+    }
+}
